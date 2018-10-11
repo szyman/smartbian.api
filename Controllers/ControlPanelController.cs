1b6e2548b7dfb23d88d5c6d3f139a97856d8a416
Marek Szymanski <szyman.marek@gmail.com>
2018-10-11 13:36:37 +0200

    Added API uploadScriptFile

--- Controllers/ControlPanelController.cs
+++ Controllers/ControlPanelController.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net.Sockets;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -14,7 +16,8 @@ namespace SmartRoomsApp.API.Controllers
     {
         private static readonly Dictionary<string, string> _COMMAND_TYPES = new Dictionary<string, string>()
         {
-            { "test_connection", "python -V" }
+            { "test_connection", "python -V" },
+            { "run_switch", "python test.py" }
         };
 
         [HttpPost("executeCommand")]
@@ -29,6 +32,47 @@ namespace SmartRoomsApp.API.Controllers
                     command.Execute();
 
                     return Ok(command.Result + command.Error);
+                }
+                catch (SocketException ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+
+        [HttpPost("uploadScriptFile")]
+        public IActionResult uploadScriptFile(ControlPanelForLoginDto controlPanelForLogin)
+        {
+            using (var client = new SftpClient(controlPanelForLogin.Host, controlPanelForLogin.Username, controlPanelForLogin.Password))
+            {
+                try
+                {
+                    string scriptFileName = "test.py";
+                    bool isFileCreated = false;
+                    client.Connect();
+                    IEnumerable<string> files = client.ListDirectory("").Select(s => s.Name);
+
+                    foreach (string fileName in files)
+                    {
+                        if (fileName == scriptFileName)
+                        {
+                            isFileCreated = true;
+                            break;
+                        }
+                    }
+
+                    if (!isFileCreated)
+                    {
+                        var stream = new MemoryStream();
+                        var writer = new StreamWriter(stream);
+                        writer.Write("print(\"This line will be printed.\")");
+
+                        writer.Flush();
+                        stream.Position = 0;
+
+                        client.UploadFile(stream, "test.py");
+                    }
+                    return Ok("File: " + scriptFileName + " exist: " + isFileCreated);
                 }
                 catch (SocketException ex)
                 {
