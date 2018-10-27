9d2310c535f15a2c07335d65ab2a0eed30a3d4b6
Marek Szymanski <szyman.marek@gmail.com>
2018-10-27 10:36:54 +0200

    Refactored SftpClient

--- Controllers/BlocksController.cs
+++ Controllers/BlocksController.cs
@@ -132,7 +132,6 @@ namespace SmartRoomsApp.API.Controllers
         [HttpGet("getScript/{blockId}")]
         public async Task<IActionResult> GetItemScript(int blockId)
         {
-            PrivateKeyFile privateKeyFile;
             Block block = await _repo.GetBlock(blockId);
 
             if (block.UserId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
@@ -140,62 +139,42 @@ namespace SmartRoomsApp.API.Controllers
 
             try
             {
-                privateKeyFile = new PrivateKeyFile(@"C:\Users\Public\private_key");
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-
-            using (var client = new SftpClient(block.User.RaspHost, block.User.RaspUsername, privateKeyFile))
-            {
-                try
+                using (var client = _getConnectedSftpClient(block.User))
                 {
                     MemoryStream stream = new MemoryStream();
-                    client.Connect();
                     client.DownloadFile(block.ScriptFileName, stream);
                     var script = Encoding.UTF8.GetString(stream.ToArray());
                     return Ok(script);
                 }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex.Message);
-                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
             }
         }
 
         [HttpPut("uploadScript/{blockId}")]
         public async Task<IActionResult> uploadScriptFile(int blockId, [FromBody]string script)
         {
-            PrivateKeyFile privateKeyFile;
             Block block = await _repo.GetBlock(blockId);
-            Stream scriptStream = this._generateStreamFromString(script);
 
             if (block.UserId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
                 return Unauthorized();
 
             try
             {
-                privateKeyFile = new PrivateKeyFile(@"C:\Users\Public\private_key");
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-
-            if (block.ScriptFileName.Length == 0)
-            {
-                block.ScriptFileName = block.UserId + "_" + block.Id + ".py";
-                await _repo.SaveAll();
-            }
-
-            using (var client = new SftpClient(block.User.RaspHost, block.User.RaspUsername, privateKeyFile))
-            {
-                try
+                if (block.ScriptFileName.Length == 0)
                 {
+                    block.ScriptFileName = block.UserId + "_" + block.Id + ".py";
+                }
+
+                using (var client = _getConnectedSftpClient(block.User))
+                {
+                    Stream scriptStream = this._generateStreamFromString(script);
+
                     string scriptFileName = block.ScriptFileName;
                     bool isFileExist = false;
-                    client.Connect();
+
                     IEnumerable<string> files = client.ListDirectory("").Select(s => s.Name);
 
                     foreach (string fileName in files)
@@ -209,17 +188,14 @@ namespace SmartRoomsApp.API.Controllers
 
                     client.UploadFile(scriptStream, scriptFileName);
                     scriptStream.Close();
+                    await _repo.SaveAll();
 
                     return Ok("File: " + scriptFileName + " Replaced: " + isFileExist);
                 }
-                catch (SocketException ex)
-                {
-                    return BadRequest(ex.Message);
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex.Message);
-                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
             }
         }
 
