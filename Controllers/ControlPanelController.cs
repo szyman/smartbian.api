e35a7a61ec357ece1f738096a600daa1bb3c2592
Marek Szymanski <szyman.marek@gmail.com>
2019-05-30 21:52:28 +0200

    Added command streaming video

--- Controllers/ControlPanelController.cs
+++ Controllers/ControlPanelController.cs
@@ -54,7 +54,7 @@ namespace SmartRoomsApp.API.Controllers
                     var commandText = await this._getCommandAsync(controlPanelForLogin.CommandType, controlPanelForLogin.ItemId);
                     client.Connect();
                     SshCommand command = client.CreateCommand(commandText);
-                    command.Execute();
+                    this._executeCommand(controlPanelForLogin.CommandType, command);
 
                     return Ok(command.Result + command.Error);
                 }
@@ -67,7 +67,7 @@ namespace SmartRoomsApp.API.Controllers
 
         private async Task<string> _getCommandAsync(string CommandType, int itemId)
         {
-            switch(CommandType)
+            switch (CommandType)
             {
                 case "test_connection":
                     return "python -V";
@@ -76,8 +76,21 @@ namespace SmartRoomsApp.API.Controllers
                     if (block == null || block.ScriptFileName == null)
                         return "";
                     return "python " + block.ScriptFileName;
+                case "video_streaming":
+                    return "raspivid -o - -t 0 -hf -w 640 -h 360 -fps 25|cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8090}' :demux=h264 | ffmpeg -i http://192.168.100.10:8090 -vcodec libx264 -f flv -r 25 -an rtmp://192.168.100.3:1935/live/stream";
                 default:
                     return "";
+            }
+        }
+
+        private void _executeCommand(string commandType, SshCommand command)
+        {
+            if (commandType == "video_streaming") {
+                var asyncExecute = command.BeginExecute();
+                command.OutputStream.CopyTo(Console.OpenStandardOutput());
+                command.EndExecute(asyncExecute);
+            } else {
+                command.Execute();
             }
         }
     }
