7f95cce6ee176e90b0d10ad9cd739f6f3f21ddcb
Marek Szymanski <szyman.marek@gmail.com>
2019-06-30 22:23:52 +0200

    Using DNS for VM

--- Controllers/ControlPanelController.cs
+++ Controllers/ControlPanelController.cs
@@ -35,8 +35,8 @@ namespace SmartRoomsApp.API.Controllers
             }
             else
             {
-                this._rtmpServerHost = "rtmp://40.68.134.134:1935";
-                this._rtmpPlaybackHost = "https://40.68.134.134:8443";
+                this._rtmpServerHost = "rtmp://smartbian.westeurope.cloudapp.azure.com:1935";
+                this._rtmpPlaybackHost = "https://smartbian.westeurope.cloudapp.azure.com:8443";
             }
             this._repo = repo;
             this._cloudStorage = cloudStorage;
